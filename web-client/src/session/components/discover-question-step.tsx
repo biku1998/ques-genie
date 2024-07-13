1b91b0f038841c2b431c53115a420eec3871b136
biku1998 <kumarsourabh228@gmail.com>
2024-07-13 18:09:04 +0530

    (feat) discover questions UI

--- web-client/src/session/components/discover-question-step.tsx
+++ web-client/src/session/components/discover-question-step.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import { useFieldArray, useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { CircleDot, Square, SquareCheck } from "lucide-react";
 import { z } from "zod";
 import StepHeading from "../../components/step-heading";
 import { Badge } from "../../components/ui/badge";
+import { Button } from "../../components/ui/button";
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
 } from "../../components/ui/form";
+import { Input } from "../../components/ui/input";
 import {
   Select,
   SelectContent,
@@ -21,6 +24,7 @@ import {
   SelectValue,
 } from "../../components/ui/select";
 import { Textarea } from "../../components/ui/textarea";
+import { cn } from "../../lib/utils";
 import { SessionQuestionSchema } from "../../types";
 import { useUpsertQuestions } from "../mutations";
 import { useFetchFullSession } from "../queries";
@@ -53,6 +57,8 @@ export default function DiscoverQuestionStep() {
     keyName: "id",
   });
 
+  const questionsWatcher = form.watch("questions");
+
   React.useEffect(() => {
     if (fetchFullSessionQuery.data) {
       form.reset({
@@ -63,7 +69,7 @@ export default function DiscoverQuestionStep() {
 
   React.useEffect(() => {
     const subscription = form.watch((value) => {
-      autoSave(value);
+      autoSave(value as z.infer<typeof FormSchema>);
     });
 
     return () => subscription.unsubscribe();
@@ -90,7 +96,7 @@ export default function DiscoverQuestionStep() {
       <StepHeading step={1} title="Discover questions ✨" />
 
       <Form {...form}>
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-8">
           {questionsFieldArr.fields.map((questionField, idx) => (
             <div
               key={questionField.id}
@@ -100,31 +106,34 @@ export default function DiscoverQuestionStep() {
                 <Badge variant="secondary" className="font-normal">
                   {getTopicNameById(questionField.topicId)}
                 </Badge>
-                <Select
-                  value={questionField.level}
-                  // onValueChange={(value) =>
-                  //   updateQuestionConfigMutation.mutate({
-                  //     sessionId,
-                  //     topicId: activeTopicId,
-                  //     id: config.id,
-                  //     type: value as QuestionType,
-                  //   })
-                  // }
-                >
-                  <SelectTrigger className="w-fit text-xs h-fit">
-                    <SelectValue placeholder="Select difficulty" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectGroup>
-                      <SelectLabel>Difficulty</SelectLabel>
-                      <SelectItem value="EASY">Easy</SelectItem>
-                      <SelectItem value="MEDIUM">Medium</SelectItem>
-                      <SelectItem value="HARD">Hard</SelectItem>
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
+
+                <FormField
+                  control={form.control}
+                  name={`questions.${idx}.level`}
+                  render={({ field }) => (
+                    <FormItem>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <FormControl>
+                          <SelectTrigger className="w-fit text-xs h-fit">
+                            <SelectValue placeholder="Select difficulty" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          <SelectGroup>
+                            <SelectLabel>Difficulty</SelectLabel>
+                            <SelectItem value="EASY">Easy</SelectItem>
+                            <SelectItem value="MEDIUM">Medium</SelectItem>
+                            <SelectItem value="HARD">Hard</SelectItem>
+                          </SelectGroup>
+                        </SelectContent>
+                      </Select>
+                    </FormItem>
+                  )}
+                />
               </div>
-              {/* <p className="text-sm font-medium">{question.text}</p> */}
 
               <FormField
                 control={form.control}
@@ -138,11 +147,93 @@ export default function DiscoverQuestionStep() {
                 )}
               />
 
-              {/* <div className="flex flex-col gap-3">
-              {question.payload.options.map((option) => (
-                <div className="flex items-center"></div>
-              ))}
-            </div> */}
+              <div className="flex flex-col gap-3 mt-4">
+                {questionField.payload.options.map((option, optionIdx) => (
+                  <div className="flex items-center gap-3">
+                    {questionField.type === "RADIO" ? (
+                      <Button
+                        size="icon"
+                        type="button"
+                        className={cn(
+                          "w-fit h-fit p-2 hover:text-emerald-500",
+                          questionsWatcher[idx].type === "RADIO" &&
+                            questionsWatcher[idx].payload.correctOptionId ===
+                              option.id
+                            ? "text-emerald-500"
+                            : "text-slate-500",
+                        )}
+                        variant="ghost"
+                        onClick={() => {
+                          form.setValue(
+                            `questions.${idx}.payload.correctOptionId`,
+                            option.id,
+                          );
+                        }}
+                      >
+                        <CircleDot size={18} />
+                      </Button>
+                    ) : (
+                      <Button
+                        size="icon"
+                        type="button"
+                        className={cn(
+                          "w-fit h-fit p-2 hover:text-emerald-500",
+                          questionsWatcher[idx].type === "CHECKBOX" &&
+                            questionsWatcher[
+                              idx
+                            ].payload.correctOptionIds.includes(option.id)
+                            ? "text-emerald-500"
+                            : "text-slate-500",
+                        )}
+                        variant="ghost"
+                        onClick={() => {
+                          if (questionsWatcher[idx].type === "RADIO") return;
+
+                          const correctOptionIds = questionsWatcher[
+                            idx
+                          ].payload.correctOptionIds.includes(option.id)
+                            ? questionsWatcher[
+                                idx
+                              ].payload.correctOptionIds.filter(
+                                (id) => id !== option.id,
+                              )
+                            : [
+                                ...questionsWatcher[idx].payload
+                                  .correctOptionIds,
+                                option.id,
+                              ];
+
+                          form.setValue(
+                            `questions.${idx}.payload.correctOptionIds`,
+                            correctOptionIds,
+                          );
+                        }}
+                      >
+                        {questionsWatcher[idx].type === "CHECKBOX" &&
+                        questionsWatcher[idx].payload.correctOptionIds.includes(
+                          option.id,
+                        ) ? (
+                          <SquareCheck size={18} />
+                        ) : (
+                          <Square size={18} />
+                        )}
+                      </Button>
+                    )}
+                    <FormField
+                      key={option.id}
+                      control={form.control}
+                      name={`questions.${idx}.payload.options.${optionIdx}.text`}
+                      render={({ field }) => (
+                        <FormItem className="w-full">
+                          <FormControl>
+                            <Input {...field} />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                ))}
+              </div>
             </div>
           ))}
         </form>
